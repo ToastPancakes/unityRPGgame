efb228ecaf3c1b799846990de59d17f0f249cc91
ToastPancakes <73261738+ToastPancakes@users.noreply.github.com>
2021-06-30 18:04:48 -0700

    Created AuNi

---
+++ Assets/Scripts/Enemy_AuNI_Script.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_AuNI_Script : MonoBehaviour
+{
+    public int randomAction;
+    public int enemySpeed;
+    public float actionTimer;
+    public float noActionTimer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomAction = Random.Range(1, 5);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        actionTimer -= Time.deltaTime;
+        noActionTimer -= Time.deltaTime;
+        if (actionTimer == 0)
+        {
+            randomAction = 0;
+            noActionTimer = 2f;
+        }
+        if (noActionTimer == 0)
+        {
+            randomAction = Random.Range(1, 5);
+        }
+
+        if (randomAction == 1)
+        {
+            transform.position += transform.up * enemySpeed * Time.deltaTime;
+            if(actionTimer <= 0)
+            {
+                actionTimer = 3f;
+            }
+            
+        }
+        if (randomAction == 2)
+        {
+            transform.position -= transform.up * enemySpeed * Time.deltaTime;
+            if (actionTimer <= 0)
+            {
+                actionTimer = 3f;
+            }
+        }
+        if (randomAction == 3)
+        {
+            transform.position += transform.right * enemySpeed * Time.deltaTime;
+            if (actionTimer <= 0)
+            {
+                actionTimer = 3f;
+            };
+        }
+        if (randomAction == 4)
+        {
+            transform.position -= transform.right * enemySpeed * Time.deltaTime;
+            if (actionTimer <= 0)
+            {
+                actionTimer = 3f;
+            }
+        }
+        if (randomAction == 5)
+        {
+            if (actionTimer <= 0)
+            {
+                actionTimer = 3f;
+            }
+
+        }
+
+
+       
+    }
+}
---
+++ Assets/Scripts/Mana_Recovery_Script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mana_Recovery_Script : MonoBehaviour
+{
+    public int manaGainAmount;
+    public GameObject player;
+    Player_Stat_Script playerStats;
+    
+
+    private void Start()
+    {
+        playerStats = FindObjectOfType<Player_Stat_Script>().GetComponent<Player_Stat_Script>();
+        manaGainAmount = playerStats.manaPoints / 4;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (player.CompareTag("Player"))
+        {
+            Player_Mana_Controller.playerMana += manaGainAmount;
+            Destroy(this.gameObject);
+        }
+
+    }
+}

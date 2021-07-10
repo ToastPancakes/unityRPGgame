93f072f13161e389c6acefa44fb1ca4b57c94ee4
ToastPancakes <73261738+ToastPancakes@users.noreply.github.com>
2021-07-10 00:17:36 -0700

    added high score system

--- Assets/Scripts/Player_Score_Script.cs
+++ Assets/Scripts/Player_Score_Script.cs
@@ -7,7 +7,7 @@ public class Player_Score_Script : MonoBehaviour
 {
     public int playerScore = 0;
     public Text inGameScore;
-   
+    public Text highScoreText;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +17,11 @@ public class Player_Score_Script : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (playerScore > PlayerPrefs.GetInt("highscore"))
+        {
+            PlayerPrefs.SetInt("highscore", playerScore);
+        }
         inGameScore.text = "Score: " + playerScore;
-        
+        highScoreText.text = "High Score: " + PlayerPrefs.GetInt("highscore");
     }
 }
